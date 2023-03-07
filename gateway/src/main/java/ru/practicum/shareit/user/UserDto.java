b5d0a63ab767be16af32aaa47a091d6ea9b9bb89
FedorScorpioN <zapevalov@yandex.ru>
2023-03-07 15:21:41 +0500

    Спринт № 16. Ревью № 1. Tests Postman. Correct Code style.

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -1,4 +1,5 @@
 package ru.practicum.shareit.user;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,8 +8,10 @@ import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.validator.Create;
 import ru.practicum.shareit.validator.Update;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
